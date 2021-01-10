a13cc3946c7e15bb85111b05408fd790e9f41a46
gbrlb <user@email.com>
2021-01-10 02:38:13 +0100

    tf2_turtle_sim whit line strip

--- src/turtle_tf2/turtle_tf2_listener.py
+++ src/turtle_tf2/turtle_tf2_listener.py
@@ -6,8 +6,20 @@ import tf2_ros
 import geometry_msgs.msg
 import turtlesim.srv
 
+from visualization_msgs.msg import Marker
+
+rospy.loginfo('Publishing example line')
+
+def wait_for_time():
+    """Wait for simulated time to begin.
+    """
+    while rospy.Time().now().to_sec() == 1:
+        pass
+
 if __name__ == '__main__':
     rospy.init_node('tf2_turtle_listener')
+
+    wait_for_time()
 
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
@@ -17,7 +29,40 @@ if __name__ == '__main__':
     turtle_name = rospy.get_param('turtle', 'turtle2')
     spawner(4, 2, 0, turtle_name)
 
+    # Publishers
     turtle_vel = rospy.Publisher('%s/cmd_vel' % turtle_name, geometry_msgs.msg.Twist, queue_size=1)
+
+    pub_turtle_path = rospy.Publisher('~turtle_path', Marker, queue_size=10)
+
+    marker = Marker()
+    marker.header.frame_id = "world"
+    marker.type = marker.LINE_STRIP
+    marker.action = marker.ADD
+
+    # marker scale
+    marker.scale.x = 0.03
+    # marker.scale.y = 0.03
+    # marker.scale.z = 0.03
+
+    # marker color
+    marker.color.a = 1.0
+    marker.color.r = 1.0
+    marker.color.g = 1.0
+    marker.color.b = 0.0
+
+    # marker orientaiton
+    marker.pose.orientation.x = 0.0
+    marker.pose.orientation.y = 0.0
+    marker.pose.orientation.z = 0.0
+    marker.pose.orientation.w = 1.0
+
+    # marker position
+    marker.pose.position.x = 0.0
+    marker.pose.position.y = 0.0
+    marker.pose.position.z = 0.0
+
+    # marker line points
+    marker.points = []
 
     rate = rospy.Rate(10.0)
     while not rospy.is_shutdown():
@@ -26,6 +71,7 @@ if __name__ == '__main__':
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             rate.sleep()
             continue
+            
 
         msg = geometry_msgs.msg.Twist()
 
@@ -33,5 +79,11 @@ if __name__ == '__main__':
         msg.linear.x = 0.5 * math.sqrt(trans.transform.translation.x ** 2 + trans.transform.translation.y ** 2)
 
         turtle_vel.publish(msg)
+
+        # second point
+        if msg.linear.x > 0.001:
+            path = tfBuffer.lookup_transform('world', turtle_name, rospy.Time())
+            marker.points.append(path.transform.translation)
+            pub_turtle_path.publish(marker)
 
         rate.sleep()
